6a180ba00ad0455c46e441f351272a7e4f079672
Naima_Bak <n.bakenchich@gmail.com>
2023-05-07 02:23:04 +0200

    random

--- random_password/src/App.js
+++ random_password/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import emailjs from '@emailjs/browser';
 import { useRef } from 'react';
 function App() {
 
@@ -13,48 +12,22 @@ function App() {
     for (i = 1; i <= 8; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
       pass += str.charAt(char);
-      // const hashedPassword = bcrypt.hashSync(pass, '$2a$10$CwTycUXWue0Thq9StjUM0u') // hash created previously created upon sign up
 
     }
     console.log(pass);
     // console.log(hashedPassword);
-    <a href="mailto:n.bakenchich@gmail.com">Click to Send an Email</a>
   }
 
 
-  const sendEmail = (e) => {
-    let user_name = "ali";
-    let user_email = "naimabenmalkek@gmail.com";
-    let subject = "send email test ";
-    let message = "hello this is the test of sending an random password"
-    e.preventDefault();
 
-    emailjs.sendForm(
-      'service_gc8x1kp',
-      'template_66vty8o',
-      form.current,
-      'W2ETjMCSG_Bx0y2NF')
-
-      .then((result) => {
-        console.log(result.text);
-      },
-        (error) => {
-          console.log(error.text);
-        });
-    // e.target.reset();
-  };
   return (
-    // <div className="App">
-    //   <form ref={form} onSubmit={sendEmail}>
-    //     <input type="text" name="" id="" />
-    //     <button onClick={generatePwd}>Generate Password </button>
-    //     <button onClick={sendEmail}> Copy Password</button>
-    //   </form>
-    <form ref={form} onSubmit={sendEmail}>
-      <input type="text" name="" id="" />
-      {/* <button onClick={generatePwd}>Generate Password </button> */}
-      <button onClick={sendEmail}> Copy Password</button>
-    </form>
+    <div className="App">
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="" id="" />
+        <button onClick={generatePwd}>Generate Password </button>
+      </form>
+    </div>
+
 
   );
 }
